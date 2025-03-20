af7273fe53b1e2cbd4d99bffa30a1eda45cf4058
ggadt <giorgino.greg@yahoo.com>
2025-03-20 16:31:15 +0100

    chore: added parsing and destructuring of values

--- src/Controller/MoneyConverterController.php
+++ src/Controller/MoneyConverterController.php
@@ -17,6 +17,39 @@ class MoneyConverterController extends AbstractController {
         #[MapQueryParameter] string $firstValue,
         #[MapQueryParameter] string $secondValue,
     ): JsonResponse {
-        return $this->json(["firstValue" => $firstValue, "secondValue" => $secondValue]);
+        //TODO: check that the query parameters are not empty, that the structure is the following
+
+        // Assert that value has the following structure:
+        // [int](pP)[int](sS)[int](dD)
+        $regex = "/([0-9]+)p([0-9]+)s([0-9]+)d/i";
+
+        $firstValueMatches = [];
+        $secondValueMatches = [];
+
+        $firstRegexMatch = preg_match($regex,$firstValue, $firstValueMatches);
+        $secondRegexMatch = preg_match($regex,$secondValue, $secondValueMatches);
+
+        // Assert that the firstValueMatches and secondValueMatches has 4 values respectively.
+
+        [$echoString, $poundsFirstValue, $shillingsFirstValue, $pencesFirstValue] = $firstValueMatches;
+        [$echoString, $poundsSecondValue, $shillingsSecondValue, $pencesSecondValue] = $secondValueMatches;
+
+
+
+        return $this->json(
+            [
+                "firstValueDestructured" => [
+                    "Pounds" => $poundsFirstValue,
+                    "Shillings" => $shillingsFirstValue,
+                    "Pences" => $pencesFirstValue,
+                ],
+                "secondValueDestructured" => [
+                    "Pounds" => $poundsSecondValue,
+                    "Shillings" => $shillingsSecondValue,
+                    "Pences" => $pencesSecondValue,
+                ]
+            ]
+        );
+
     }
 }
