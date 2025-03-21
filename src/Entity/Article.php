b146c085d899444fcca0607940d1bbd0a6f9de78
ggadt <giorgino.greg@yahoo.com>
2025-03-21 16:26:34 +0100

    chore: removed setId as it's automatic, added table name

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,6 +6,7 @@ use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\Table(name: "articles")]
 class Article
 {
     #[ORM\Id]
@@ -22,13 +23,6 @@ class Article
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function setId(int $id): static
-    {
-        $this->id = $id;
-
-        return $this;
     }
 
     public function getName(): ?string
