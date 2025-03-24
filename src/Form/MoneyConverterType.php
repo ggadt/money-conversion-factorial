ac722e7ac7ea136687ec588214d9803e6835b74e
ggadt <giorgino.greg@yahoo.com>
2025-03-24 14:28:51 +0100

    chore: added future useful form validations

--- src/Form/MoneyConverterType.php
+++ src/Form/MoneyConverterType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Validator\IsValidAmount;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -10,27 +11,26 @@ use Symfony\Component\PropertyInfo\Type;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class MoneyConverterType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class MoneyConverterType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
             ->add('firstValue', TextType::class, [
                 'constraints' => [
                     new NotBlank(),
-                    new Assert\Type('string')
+                    new Assert\Type('string'),
+                    new IsValidAmount()
                 ]
             ])
             ->add('secondValue', TextType::class, [
                 'constraints' => [
                     new NotBlank(),
-                    new Assert\Type('string')
+                    new Assert\Type('string'),
+                    new IsValidAmount()
                 ]
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'allow_extra_fields' => false
         ]);
--- src/Service/MoneyConverterService.php
+++ src/Service/MoneyConverterService.php
@@ -8,29 +8,25 @@ class MoneyConverterService {
 
     /**
      * This function expects two parameters:
-     *  @param Amount $firstAmount
-     *  @param Amount $secondAmount
+     * @param  Amount  $firstAmount
+     * @param  Amount  $secondAmount
      *
      * @return Amount $sumAmounts The sum with the first amount added to the second one.
      */
-    public static function sumAmounts(Amount $firstAmount, Amount $secondAmount): Amount
-    {
+    public static function sumAmounts(Amount $firstAmount, Amount $secondAmount): Amount {
         return Amount::sum($firstAmount, $secondAmount);
     }
 
-    public static function subtractAmounts(Amount $firstAmount, Amount $secondAmount): Amount
-    {
+    public static function subtractAmounts(Amount $firstAmount, Amount $secondAmount): Amount {
         return Amount::subtraction($firstAmount, $secondAmount);
     }
 
-    public static function multiplyAmounts(Amount $amount, int $multiplier): Amount
-    {
+    public static function multiplyAmounts(Amount $amount, int $multiplier): Amount {
         return Amount::multiplication($amount, $multiplier);
     }
 
 
-    public static function divideAmount(Amount $amount, int $divider): array
-    {
+    public static function divideAmount(Amount $amount, int $divider): array {
         return Amount::division($amount, $divider);
     }
 }
