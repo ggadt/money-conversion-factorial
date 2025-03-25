6a0f47a6952157ddf7f3ab339fec974244c73463
ggadt <giorgino.greg@yahoo.com>
2025-03-25 17:42:31 +0100

    feat: added tests for old Money Calculator

--- tests/Controller/Api/v1/OldMoneyCalculatorControllerTest.php
+++ tests/Controller/Api/v1/OldMoneyCalculatorControllerTest.php
@@ -21,6 +21,7 @@ class OldMoneyCalculatorControllerTest extends TestCase {
             ]
         ]);
     }
+
     protected function tearDown(): void {
         $this->client = null;
     }
@@ -47,159 +48,163 @@ class OldMoneyCalculatorControllerTest extends TestCase {
      * @throws GuzzleException
      */
     public function testSubtractMoney() {
-          $response = $this->client->get($this->baseUrl.'/subtraction', [
-              "query" => [
-                  'firstValue' => '5p17s8d',
-                  'secondValue' => '3p4s10d',
-              ]
-          ]);
+        $response = $this->client->get($this->baseUrl.'/subtraction', [
+            "query" => [
+                'firstValue' => '5p17s8d',
+                'secondValue' => '3p4s10d',
+            ]
+        ]);
 
-          $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
-          $this->assertJsonResponse($response->getBody(), [
-              'result' => '2p12s10d',
-          ]);
-      }
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJsonResponse($response->getBody(), [
+            'result' => '2p12s10d',
+        ]);
+    }
 
-    //  // Test: Moltiplicazione con un intero
-    //  public function testMultiplyMoneyByInteger() {
-    //      $client = $this->client;
-//
-//
-    //      // Moltiplicazione di un valore monetario per un intero
-    //      $client->request('GET', $this->baseUrl.'/multiplication', [
-    //          'firstValue' => '5p17s8d',
-    //          'multiplier' => 2,
-    //      ]);
-//
-    //      $this->assertResponseIsSuccessful();
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'result' => '11p15s4d',
-    //      ]);
-    //  }
-//
-    //  // Test: Divisione con resto (valore piccolo)
-    //  public function testDivideMoneyWithRemainder() {
-    //      $client = $this->client;
-//
-//
-    //      // Divisione con resto
-    //      $client->request('GET', $this->baseUrl.'/division', [
-    //          'firstValue' => '5p17s8d',
-    //          'divider' => 2,
-    //      ]);
-//
-    //      $this->assertResponseIsSuccessful();
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'result' => '1p5s0d',
-    //          'remainder' => '0p1s1d',
-    //      ]);
-    //  }
-//
-    //  // Test: Divisione per zero
-    //  public function testDivideByZero() {
-    //      $client = $this->client;
-//
-//
-    //      // Divisione per zero
-    //      $client->request('GET', $this->baseUrl.'/division', [
-    //          'firstValue' => '5p17s8d',
-    //          'divisor' => 0,
-    //      ]);
-//
-    //      $this->assertResponseStatusCodeSame(400); // Errore 400 per divisione per zero
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'error' => 'Divisione per zero non consentita',
-    //      ]);
-    //  }
-//
-    //  // Test: Somma di numeri grandi
-//
-    //  public function testAddLargeNumbers() {
-    //      $client = $this->client;
-//
-//
-    //      // Somma tra numeri molto grandi
-    //      $client->request('GET', $this->baseUrl.'/sum', [
-    //          'firstValue' => '9999999p59s999d',
-    //          'secondValue' => '1000000p40s5d',
-    //      ]);
-//
-    //      $this->assertResponseIsSuccessful();
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'result' => '10999999p99s1004d',
-    //      ]);
-    //  }
-//
-    //  // Test: Sottrazione con numeri negativi (debito)
-    //  public function testSubtractNegativeResult() {
-    //      $client = $this->client;
-//
-//
-    //      $client->request('GET', $this->baseUrl.'/subtraction', [
-    //          'firstValue' => '5p0s0d',
-    //          'secondValue' => '5p17s8d',
-    //      ]);
-//
-    //      $this->assertResponseIsSuccessful();
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'result' => '0p0s0d',
-    //      ]);
-    //  }
-//
-    //  // Test: Moltiplicazione con zero
-    //  public function testMultiplyByZero() {
-    //      $client = $this->client;
-//
-//
-    //      // Moltiplicazione per zero
-    //      $client->request('GET', $this->baseUrl.'/subtraction', [
-    //          'firstValue' => '5p17s8d',
-    //          'multiplier' => 0,
-    //      ]);
-//
-    //      $this->assertResponseIsSuccessful();
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'result' => '0p0s0d',
-    //      ]);
-    //  }
-//
-    //  // Test: Divisione con resto (valore molto piccolo)
-    //  public function testDivideWithSmallRemainder() {
-    //      $client = $this->client;
-//
-//
-    //      // Divisione con un resto molto piccolo
-    //      $client->request('GET', $this->baseUrl.'/division', [
-    //          'firstValue' => '2p1s1d',
-    //          'divisor' => 3,
-    //      ]);
-//
-    //      $this->assertResponseIsSuccessful();
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'result' => '0p17s0d',
-    //          'remainder' => '0p1s1d',
-    //      ]);
-    //  }
-//
-    //  // Test: Somma di valori con formati errati
-    //  public function testAddInvalidFormat() {
-    //      $client = $this->client;
-//
-//
-    //      // Somma con formato errato
-    //      $client->request('GET', $this->baseUrl.'/sum', [
-    //          'firstValue' => '5p10x8d', // Formato errato
-    //          'secondValue' => '3p4s10d',
-    //      ]);
-//
-    //      $this->assertResponseStatusCodeSame(400); // Errore 400 per formato errato
-    //      $this->assertJsonResponse($client->getResponse(), [
-    //          'error' => 'Formato non valido per il primo valore',
-    //      ]);
-    //  }
-//
-    // Funzione di utilità per verificare la risposta JSON
-    private function assertJsonResponse($response, $expectedData) {
+    /**
+     * @throws GuzzleException
+     */
+    public function testMultiplyMoneyByInteger() {
+        $response = $this->client->get($this->baseUrl.'/multiplication', [
+            "query" => [
+                'firstValue' => '5p17s8d',
+                'multiplier' => '3',
+            ]
+        ]);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJsonResponse($response->getBody(), [
+            'result' => '17p13s0d',
+        ]);
+    }
+
+    public function testDivideMoneyWithRemainder() {
+        $response = $this->client->get($this->baseUrl.'/division', [
+            "query" => [
+                'firstValue' => '18p16s1d',
+                'divider' => '15',
+            ]
+        ]);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJsonResponse($response->getBody(), [
+            'result' => '1p5s0d',
+            'remainder' => '0p1s1d',
+        ]);
+    }
+
+    // Test: Divisione per zero
+
+    /**
+     * @throws GuzzleException
+     */
+    public function testDivideByZero() {
+        try {
+            $this->client->get($this->baseUrl.'/division', [
+                "query" => [
+                    'firstValue' => '18p16s1d',
+                    'divider' => '0',
+                ]
+            ]);
+
+        } catch (GuzzleException $e) {
+            $response = $e->getResponse();
+            $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+            $responseDecodified = json_decode($response->getBody()->getContents(), true);
+            $this->assertArrayHasKey("error", $responseDecodified);
+            $this->assertArrayHasKey("errors", $responseDecodified);
+        }
+    }
+
+    // Test: Somma di numeri grandi
+    public function testAddLargeNumbers() {
+        $response = $this->client->get($this->baseUrl.'/sum', [
+            "query" => [
+                'firstValue' => '9999999p59s999d',
+                'secondValue' => '1000000p40s5d',
+            ]
+        ]);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJsonResponse($response->getBody(), [
+            'result' => '11000008p2s8d',
+        ]);
+    }
+
+    // Test: Sottrazione con numeri negativi (debito)
+    public function testSubtractNegativeResult() {
+        try {
+            $this->client->get($this->baseUrl.'/subtraction', [
+                "query" => [
+                    'firstValue' => '5p17s8d',
+                    'secondValue' => '6p4s10d',
+                ]
+            ]);
+        } catch (GuzzleException $e) {
+            $response = $e->getResponse();
+            $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+            $responseDecodified = json_decode($response->getBody()->getContents(), true);
+            $this->assertArrayHasKey("error", $responseDecodified);
+        }
+    }
+
+    // Test: Moltiplicazione con zero
+
+    /**
+     * @throws GuzzleException
+     */
+    public function testMultiplyByZero() {
+
+        $response = $this->client->get($this->baseUrl.'/multiplication', [
+            "query" => [
+                'firstValue' => '18p16s1d',
+                'multiplier' => '0',
+            ]
+        ]);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJsonResponse($response->getBody(), [
+            'result' => '0p0s0d',
+        ]);
+    }
+
+    // Test: Divisione con resto (valore piccolo)
+    public function testDivideWithSmallRemainder() {
+        try {
+            $this->client->get($this->baseUrl.'/division', [
+                "query" => [
+                    'firstValue' => '18p16s1d',
+                    'divider' => '0',
+                ]
+            ]);
+        } catch (GuzzleException $e) {
+            $response = $e->getResponse();
+            $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+            $responseDecodified = json_decode($response->getBody()->getContents(), true);
+            $this->assertArrayHasKey("error", $responseDecodified);
+        }
+    }
+
+    // Test: Somma di valori con formati errati
+    public function testAddInvalidFormat() {
+        try {
+            $this->client->get($this->baseUrl.'/sum', [
+                "query" => [
+                    'firstValue' => '5p10x8d', // Formato errato
+                    'secondValue' => '3p4s10d',
+                ]
+            ]);
+        } catch (GuzzleException $e) {
+            $response = $e->getResponse();
+            $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        }
+    }
+
+// Funzione di utilità per verificare la risposta JSON
+    private function assertJsonResponse(
+        $response,
+        $expectedData
+    ) {
         $data = json_decode($response, true);
         $this->assertEquals($expectedData, $data);
     }
